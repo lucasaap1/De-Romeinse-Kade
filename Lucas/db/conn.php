29d6644a4158145270e23f688de0c4406d815c0d
Jouw Naam <jouw-email@example.com>
2026-05-27 14:41:23 +0200

    Eerste upload website

---
+++ Lucas/db/conn.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'config.php';
+
+class db_conn{
+
+
+}
---
+++ Lucas/prefabs/navbar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styling/styling.css">
+
+
+  <title>navbar</title>
+</head>
+
+<body>
+
+  <nav>
+    <ul>
+      <li><a href="../index.php">Home</a></li>
+      <li><a href="../pages/Menu">Menukaart</a></li>
+      <li><a href="../pages/Reserveren">Reserveren</a></li>
+      <li><a href="../pages/Openingstijden">Openingstijden</a></li>
+      <li><a href="../pages/Over_Ons">Over ons</a></li>
+      <li><a href="../pages/Contact">Contact</a></li>
+    </ul>
+  </nav>
+</body>
+
+</html>
