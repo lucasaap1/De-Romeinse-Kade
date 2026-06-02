f430f2523736670f6694a568f97a4cbf63793eec
Jouw Naam <jouw-email@example.com>
2026-06-02 12:25:19 +0200

    Database OOP

---
+++ Lucas/pages/menu.php
@@ -0,0 +1,37 @@
+<?php 
+
+$db = include_once('../De-Romeinse-Kade/main/db/db.php');
+$statement = $db->prepare(""); 
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Links -->
+    <link rel="stylesheet" href="../../Lucas/styling/styling.css">
+    <!--Title -->
+    <title>Menu</title>
+</head>
+
+<body>
+    <!--navbar -->
+    <?php include('../../Lucas/prefabs/navbar.php') ?>
+
+    <!--menu-->
+    <div class="menu-container">
+        <?php
+        for(){
+
+        }
+        ?>
+
+    </div>
+</body>
+
+</html>
