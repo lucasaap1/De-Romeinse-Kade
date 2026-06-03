8d6bc01518779ac5840fdc0db2fcf97030f71165
dakotah <dakotah.vloet@edu-kw1c.nl>
2026-06-03 14:11:49 +0200

    account registratie gemaakt

---
+++ Dakotah/class/accountClass.php
@@ -0,0 +1,25 @@
+<?php
+require_once ('../../../De-Romeinse-Kade/main/db/db.php');
+class Account
+{
+    public function register($username, $email, $password)
+    {
+        $dbClass = new Database();
+        $db = $dbClass->connection();
+
+        $stmt = $db->prepare("INSERT INTO accounts (naam, email, wachtwoord) VALUES (:username, :email, :password)");
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $password);
+        $stmt->execute();
+    }
+
+    public function login($email, $password)
+    {
+    }
+
+    public function logout()
+    {
+    }
+}
+?>
---
+++ Dakotah/class/classmenu.php
@@ -0,0 +1,23 @@
+<?php
+require_once '../../../De-Romeinse-Kade/main/db/db.php';
+
+class Menu
+{
+    public function readItems()
+    {
+        $dbClass = new Database();
+        $db = $dbClass->connection();
+
+        $stmt = $db->prepare("SELECT * FROM items");
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+    public function addToMenu() {}
+
+    public function deleteItemFromMenu() {
+        
+    }
+}
