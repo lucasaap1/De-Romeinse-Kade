d0dd397b449ff2c52ae4b9b3e286f8b07e32b02d
Jouw Naam <jouw-email@example.com>
2026-06-03 09:13:43 +0200

    Knoppen voeg toe, update, delete (nog geen code)

--- Lucas/pages/menu.php
+++ Lucas/pages/menu.php
@@ -16,8 +16,8 @@ $db = $dbClass->connection();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<!--links -->   
-<link rel="stylesheet" href="../../Lucas/styling/styling.css">
+    <!--links -->
+    <link rel="stylesheet" href="../../Lucas/styling/styling.css">
     <title>Menu</title>
 </head>
 
@@ -27,16 +27,23 @@ $db = $dbClass->connection();
     <?php include('../../Lucas/prefabs/navbar.php') ?>
 
 
-<!--Menu -->
-<div class="menu-container">
-    <?php foreach ($items as $item): ?>
-        <div class="menu-item">
-            <h3><?= htmlspecialchars($item['item']) ?></h3>
-            <p><?= htmlspecialchars($item['omschrijving']) ?></p>
-            <p>€<?= htmlspecialchars($item['prijs']) ?></p>
-        </div>
-    <?php endforeach; ?>
-</div>
+    <!--Menu -->
+    <div class="menu-container">
+        <?php foreach ($items as $item): ?>
+            <div class="menu-item">
+                <h3><?= htmlspecialchars($item['item']) ?></h3>
+                <p><?= htmlspecialchars($item['omschrijving']) ?></p>
+                <p>€<?= htmlspecialchars($item['prijs']) ?></p>
+
+                <form action="menu.php" method="post">
+                    <button class="add">Voeg toe</button>
+                    <button class="update">Verander eten</button>
+                    <button class="delete">Verwijder</button>
+
+                </form>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </body>
 
 </html>
