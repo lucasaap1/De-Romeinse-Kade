371e7be933912feefc43521456052d166f8ab1c1
Jouw Naam <jouw-email@example.com>
2026-06-04 09:10:50 +0200

    Bestelling toevoegen aan de database

--- Lucas/prefabs/navbar.php
+++ Lucas/prefabs/navbar.php
@@ -1,44 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <title>De Romeinse Kade</title>
-</head>
-
-<body>
-  <nav class="rk-nav" role="navigation" aria-label="Hoofdnavigatie">
+<nav class="rk-nav" role="navigation" aria-label="Hoofdnavigatie">
     <a class="rk-brand" href="#">De Romeinse<br>Kade</a>
 
     <div class="rk-links" id="desktopLinks">
-      <a href="../../../De-Romeinse-Kade/Lucas/index.php">Home</a>
-      <a href="../../../De-Romeinse-Kade/Lucas/pages/menu.php">Menukaart</a>
-      <a href="../../../De-Romeinse-Kade/Lucas/pages/reserve.php">Reserveren</a>
-      <a href="../../../De-Romeinse-Kade/Lucas/pages/about.php">Over ons</a>
-      <a href="../../../De-Romeinse-Kade/Lucas/pages/contact.php">Contact</a>
+        <a href="../../../De-Romeinse-Kade/Lucas/index.php">Home</a>
+        <a href="../../../De-Romeinse-Kade/Lucas/pages/menu.php">Menukaart</a>
+        <a href="../../../De-Romeinse-Kade/Lucas/pages/reserve.php">Reserveren</a>
+        <a href="../../../De-Romeinse-Kade/Lucas/pages/about.php">Over ons</a>
+        <a href="../../../De-Romeinse-Kade/Lucas/pages/contact.php">Contact</a>
     </div>
 
     <div class="rk-right">
-      <button class="rk-dark-btn" id="darkToggle" aria-label="Donkere modus aan/uit">
-        <i class="ti ti-moon" aria-hidden="true"></i>
-      </button>
-      <button class="rk-burger" id="burgerBtn" aria-label="Menu openen">
-        <i class="ti ti-menu-2" aria-hidden="true"></i>
-      </button>
+        <button class="rk-dark-btn" id="darkToggle" aria-label="Donkere modus aan/uit">
+            <i class="ti ti-moon" aria-hidden="true"></i>
+        </button>
+        <button class="rk-burger" id="burgerBtn" aria-label="Menu openen">
+            <i class="ti ti-menu-2" aria-hidden="true"></i>
+        </button>
     </div>
-  </nav>
+</nav>
 
-  <div class="rk-mobile-menu" id="mobileMenu">
+<div class="rk-mobile-menu" id="mobileMenu">
     <a href="#" class="active">Home</a>
     <a href="#">Menukaart</a>
     <a href="#">Reserveren</a>
     <a href="#">Openingstijden</a>
     <a href="#">Over ons</a>
     <a href="#">Contact</a>
-  </div>
-
-</body>
-
-</html>
+</div>
