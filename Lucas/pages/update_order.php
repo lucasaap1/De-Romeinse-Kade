cb9a76c1b91a4d11c4c70bc64168f5fb1641ae25
Jouw Naam <jouw-email@example.com>
2026-06-11 12:01:31 +0200

    Aantal kunnen toevoegen in bestellingen van de popup

--- Lucas/pages/update_order.php
+++ Lucas/pages/update_order.php
@@ -4,9 +4,9 @@ session_start();
 require_once '../class/classMenu.php';
 
 // Hardcoded for testing, replace with $_SESSION['klant_id'] when login is done
-$customerId = 2;
+$customerId = 2;    
 
-// Get data from request
+// Get data from req
 $data = json_decode(file_get_contents('php://input'), true);
 $itemId = (int)($data['item_id'] ?? 0);
 $aantal = (int)($data['aantal'] ?? 0);
@@ -15,7 +15,7 @@ $aantal = (int)($data['aantal'] ?? 0);
 $cart = new ShoppingCart();
 $cart->deleteAllOfItem($customerId, $itemId);
 
-// Re-add item the correct number of times
+// Add amount to db
 for ($i = 0; $i < $aantal; $i++) {
     $cart->addToOrder($customerId, $itemId);
 }
